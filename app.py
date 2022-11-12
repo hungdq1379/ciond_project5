3c94f53a8d1b7ad44c9a4c59090277e49b79ce4c
hungdq1379 <hungdq1379@gmail.com>
2022-11-12 16:21:35 +0700

    build iamge

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello():
     return 'Hello World. My name is Duong Quoc Hung'
  
-app.run(host="0.0.0.0", port=80, debug=True).
+app.run(host="0.0.0.0", port=80, debug=True)
